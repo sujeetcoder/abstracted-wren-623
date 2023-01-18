6930a7773db1ab7088bc5872cb262cab79ebdfe4
AviAndhariya <avi.andhariya@gmail.com>
2023-01-18 16:13:51 +0530

    carousel

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,12 @@ import { useMedia } from "./MediaQuery/UseMedia";
 import AllRoutes from "./Routes/AllRoutes";
 
 function App() {
-  const {mediumScreen} = useMedia()
+  const { mediumScreen } = useMedia();
   return (
     <Box className="App">
-      {/* {mediumScreen? <Nav/> : <NavMobile/>} */}
-      {/* <Nav /> */}
-      <NavMobile />
-
+      {mediumScreen ? <Nav /> : <NavMobile />}
+      {/* <Nav />
+      <NavMobile />  */}
       <AllRoutes />
     </Box>
   );
--- client/src/Routes/AllRoutes.jsx
+++ client/src/Routes/AllRoutes.jsx
@@ -1,7 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import SingleProduct from "../Components/SingleProduct/SingleProduct";
-import Home from "../Pages/Home";
-import SingleProduct from '../Components/SingleProduct/SingleProduct'
 import TelevisionData from '../Components/Television/TelevisionData'
 import Home from '../Pages/Home'
 import Products from "../Components/Products/Products";
