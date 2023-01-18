a74de55bcb4b4c6eb06e9c8654de3ae121d83055
AviAndhariya <avi.andhariya@gmail.com>
2023-01-18 13:54:08 +0530

    products page

--- client/src/Routes/AllRoutes.jsx
+++ client/src/Routes/AllRoutes.jsx
@@ -1,16 +1,15 @@
-import { Route, Routes } from 'react-router-dom'
-import SingleProduct from '../Components/SingleProduct/SingleProduct'
-import Home from '../Pages/Home'
-
+import { Route, Routes } from "react-router-dom";
+import SingleProduct from "../Components/SingleProduct/SingleProduct";
+import Home from "../Pages/Home";
 
 const AllRoutes = () => {
   return (
     <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="*" element={<h1>Hello</h1>}></Route>
-       <Route path='/singleproduct' element={<SingleProduct />} />
+      <Route path="/" element={<Home />}></Route>
+      <Route path="*" element={<h1>Hello</h1>}></Route>
+      <Route path="/singleproduct" element={<SingleProduct />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default AllRoutes
+export default AllRoutes;
