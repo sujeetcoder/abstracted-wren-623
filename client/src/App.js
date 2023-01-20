91bce65f89e48c3328298c2f7c10757b1b2e5f48
Sujeet Kumar <sujeetkumaryadav202020@gmail.com>
2023-01-20 14:51:54 +0530

    login page compaleted

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,14 @@ import Nav from "./Components/Navbar/Nav";
 import NavMobile from "./Components/Navbar/NavMobile/NavMobile";
 import { useMedia } from "./MediaQuery/UseMedia";
 import AllRoutes from "./Routes/AllRoutes";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   const { mediumScreen } = useMedia();
   return (
     <Box className="App">
       {mediumScreen?<Nav/>:<NavMobile/>}
-      <Box h="40px"></Box>
+      <Box h="50px"></Box>
       <AllRoutes />
       <Footer />
     </Box>
