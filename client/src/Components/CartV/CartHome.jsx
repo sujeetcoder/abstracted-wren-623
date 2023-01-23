9cf1047d37521307ea151a21685ac8c74ab38bd8
vishnu9651 <vishnukantee0044@gmail.com>
2023-01-23 11:45:51 +0530

    added

--- client/src/Components/CartV/CartHome.jsx
+++ client/src/Components/CartV/CartHome.jsx
@@ -43,9 +43,11 @@ useEffect(() => {
 
 
     
+console.log( "njvnjdvd" ,cartData[0])
 
-
-
+const cartTotal=localStorage.getItem("total")
+    console.log("yy",cartTotal)
+    
 
 
     return (<div >
@@ -54,7 +56,7 @@ useEffect(() => {
 
 
 
-        <Box  padding={"5%"} justifyItems={"center"} fontWeight={"light"} textAlign={"center"}><Heading>Your bag total is ₹92400.00 or ₹10963.00/mo.</Heading>
+        <Box  padding={"5%"} justifyItems={"center"} fontWeight={"light"} textAlign={"center"}><Heading>Your bag total is ₹{cartTotal>0?cartTotal:"00"} </Heading>
         <Box><Button marginTop={"3%"}  width={"30%"} borderRadius={"7px"} backgroundColor={"#0071e3"} color="white">Check Out</Button></Box>
         
         </Box>
@@ -62,7 +64,7 @@ useEffect(() => {
         
         
         <hr />
-        {cartData && cartData.map((el)=>(<div key={el._id}> <Box marginTop={"5px"} ><CartItem delPro={delPro} setchangeone={setchangeone} {...el}/></Box> </div>))}
+        {cartData && cartData.map((el)=>(<div key={el._id}> <Box marginTop={"5px"} ><CartItem delPro={delPro} setchangeone={setchangeone}  {...el}/></Box> </div>))}
         <br />
         
         
--- client/src/Components/CartV/CartItem.jsx
+++ client/src/Components/CartV/CartItem.jsx
@@ -7,18 +7,21 @@ import { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
 import { dataUrl } from "../../Utils/AllUrls";
-export const CartItem = ({_id,product,quantity,delPro,setchangeone}) => {
-    const {name, image, price} = product
-    const changeHandler1 = (value,_id) => {
-        console.log(_id,typeof value)
-        axios.patch(`${dataUrl}/carts/change/${_id}`,{quantity:value},{withCredentials:true}).then((res)=>{
-            setchangeone((pr)=>pr+1)
+var total=0
+export const CartItem = ({ _id, product, quantity, delPro, setchangeone }) => {
+
+    const { name, image, price } = product
+    const changeHandler1 = (value, _id) => {
+        console.log(_id, typeof value)
+        axios.patch(`${dataUrl}/carts/change/${_id}`, { quantity: value }, { withCredentials: true }).then((res) => {
+            setchangeone((pr) => pr + 1)
             console.log("success")
-        }).catch((err)=>console.log(err.message))
+        }).catch((err) => console.log(err.message))
     }
-
-
-
+console.log("p",price*quantity)
+ total=total+price*quantity
+console.log("total",total)
+localStorage.setItem("total",total)
 
 
 
@@ -32,22 +35,22 @@ export const CartItem = ({_id,product,quantity,delPro,setchangeone}) => {
         <Box width="100%" paddingTop="5%" lineHeight={"25px"} justifyContent={"center"} justifyItems={"center"} justifySelf={"auto"} margin={"auto"} >
             <Flex>
                 <Box boxSize={"18%"}  ><Img src={image[0]} alt="img" /></Box>
-                <Box  width="90%" >
+                <Box width="90%" >
                     <Box >
                         <Flex justifyContent={"space-between"}>
                             <Box width={"45%"}>
                                 <Text fontSize={"25px"} fontWeight={"medium"} >
                                     {name}</Text></Box>
-                            <Box marginRight={"25%"}><Select onChange={(e)=>changeHandler1(+e.target.value,_id)} defaultValue={quantity}><option value={1}>1</option>
-                            
-                            <option value={2}>2</option>
-                            <option value={3}>3</option>
-                            <option value={4}> 4</option>
+                            <Box marginRight={"25%"}><Select onChange={(e) => changeHandler1(+e.target.value, _id)} defaultValue={quantity}><option value={1}>1</option>
 
-                            
+                                <option value={2}>2</option>
+                                <option value={3}>3</option>
+                                <option value={4}> 4</option>
+
+
                             </Select></Box>
                             <Box>
-                                <Box fontWeight={"medium"} fontSize={"2xl"}>{price*quantity}</Box>
+                                <Box fontWeight={"medium"} fontSize={"2xl"}>{price * quantity}</Box>
                             </Box>
                         </Flex>
                     </Box>
@@ -55,10 +58,10 @@ export const CartItem = ({_id,product,quantity,delPro,setchangeone}) => {
                         <Box>Pay 16% pa for 6 months</Box>
                         <Box> ₹8692.00/mo.</Box>
                     </Flex></Box>
-                    <Box textAlign={"end"} marginTop={"2%"} marginBottom="2%" color="blue" fontSize={"lg"} onClick={()=>delPro(_id)}>Remove</Box>
+                    <Box textAlign={"end"} marginTop={"2%"} marginBottom="2%" color="blue" fontSize={"lg"} onClick={() => delPro(_id)}>Remove</Box>
                     <hr />
                     <hr />
-                    <Box paddingTop={"15px"} paddingBottom="15px"  fontSize="lg" >
+                    <Box paddingTop={"15px"} paddingBottom="15px" fontSize="lg" >
                         <Flex justifyContent={"space-between"}>
                             <Box> <Flex gap={"10px"}><Box marginTop={"5px"}><HiOutlineGiftTop size={"20px"} /></Box>
                                 <Box fontWeight={"medium"}>Add a gift message</Box></Flex></Box>
@@ -67,10 +70,10 @@ export const CartItem = ({_id,product,quantity,delPro,setchangeone}) => {
                     </Box>
                     <hr />
                     <hr />
-            
+
                     <Box >
                         <Flex gap={"3px"} marginTop="8px"><Box><Text fontWeight={"medium"} fontSize="md" marginTop={"5px"}>Find out how soon you can get this Item</Text></Box>
-                      <Box> <Text color={"blue"} marginTop="6px">Enter PinCode </Text> </Box><Box marginTop={"8px"}><MdKeyboardArrowDown color="blue" /></Box></Flex>
+                            <Box> <Text color={"blue"} marginTop="6px">Enter PinCode </Text> </Box><Box marginTop={"8px"}><MdKeyboardArrowDown color="blue" /></Box></Flex>
                         <Flex gap="5px"><Box marginTop={"5px"} > <BsBoxSeam /></Box><Box><Text> In stock ready to ship</Text></Box></Flex>
                     </Box>
                 </Box>
@@ -79,7 +82,7 @@ export const CartItem = ({_id,product,quantity,delPro,setchangeone}) => {
             <br />
             <hr />
             <hr />
-            
+
         </Box>
     </div>)
 }
